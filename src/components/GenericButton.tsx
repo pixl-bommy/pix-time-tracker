4e6fa16bd1c2c69b04f7b2e15fd5d30b6100e1fe
Bommy <git@bommy.net>
2021-03-09 21:14:00 +0100

    created GenericButton

---
+++ src/components/GenericButton.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from "react";
+import "./GenericButton.scss";
+
+interface GenericButtonProps {
+   children?: ReactNode;
+   selected?: boolean;
+   size?: "quater" | "half" | "full";
+   onClick?: () => void;
+}
+
+function GenericButton({ children, selected, size, onClick }: GenericButtonProps): JSX.Element {
+   const cssClasses = ["GenericButton", selected && "selected", size || "full"]
+      .filter((x) => x)
+      .join(" ");
+
+   return (
+      <button className={cssClasses} onClick={onClick}>
+         {children}
+      </button>
+   );
+}
+
+export default GenericButton;
