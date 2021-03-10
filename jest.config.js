72ebb7967283bead3e9c465541e91eec5725bc92
Bommy <git@bommy.net>
2021-03-10 20:58:00 +0100

    added jest for testing

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+var IGNORE_PATTERNS = ["/node_modules/", "/dist/"];
+
+module.exports = {
+   preset: "ts-jest",
+   testEnvironment: "node",
+   coverageDirectory: "./.reports/coverage",
+   collectCoverageFrom: ["**/*.ts", "**/*.tsx"],
+   coveragePathIgnorePatterns: IGNORE_PATTERNS,
+   testMatch: ["**/*.test.ts", "**/*.test.tsx"],
+   testPathIgnorePatterns: IGNORE_PATTERNS,
+   moduleNameMapper: {
+      "@/(.*)": ["<rootDir>/src/$1"],
+      "\\.(scss|css)$": "<rootDir>/mocks/stylesheet.mock.js",
+   },
+};
