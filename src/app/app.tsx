6bae1e2f77d5f56e0be20e355a05a4462d1f3dee
Bommy <git@bommy.net>
2021-03-12 20:57:29 +0100

    refactor: mainmenu is not a page anymore

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,6 @@
-import Mainmenu from "@/pages/Mainmenu";
 import React, { useState } from "react";
 
+import Button from "../components/GenericButton";
 import Tracker from "../pages/tracker";
 
 import "./app.scss";
@@ -13,7 +13,13 @@ function App({ initialActions }: { initialActions: Map<string, string> }): JSX.E
    return (
       <>
          <div className="app">
-            <Mainmenu setOverlay={setOverlay} />
+            <h1>pixl Time Tracker</h1>
+            <div className="Mainmenu">
+               <Button color="#00f" textcolor="#fff" onClick={() => setOverlay("tracker")}>
+                  Tracker Today
+               </Button>
+            </div>
+            <div style={{ flexGrow: 1 }}></div>
          </div>
 
          <div className={`${overlay === "tracker" && "slide-in"} fullscreen-overlay`}>
